f8d7cfa10ba832a38347eb5c61c69b5da3e84806
simholmen <seholmen@student.uia.no>
2025-04-30 12:58:33 +0200

    fiksa callprocedures.sql

--- callprocedures.sql
+++ callprocedures.sql
@@ -1,6 +1,6 @@
 --Purchase membership stored procedure
 --This procedure will add a new membership to the BC_MEMBERSHIP table
-CALL purchase_membership_sp(NULL, 'Heartland Monthly Pass', 20, DATE'2022-01-19', DATE'2022-01-19', 1)
+CALL purchase_membership_sp(NULL, 'Heartland Monthly Pass', 20, DATE'2022-01-19', DATE'2022-01-19', 10)
 --This procedure will add a new bicycle to the BC_Bicycle table
 CALL create_bicycle_sp(NULL, 'classic',null, null, null, null, 2023, 'available', 40, 40,50, 50, null)
 --This procedure will add a new account to the BC_Account table
